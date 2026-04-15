f6d26fa8a5650c4269919e6ec9681ec9b5764bae
Артем Савкин <58187114+OrangeRedeng@users.noreply.github.com>
2026-04-15 17:37:23 +0300

    Update deepep.py

--- python/sglang/srt/layers/moe/token_dispatcher/deepep.py
+++ python/sglang/srt/layers/moe/token_dispatcher/deepep.py
@@ -387,7 +387,6 @@ class _DeepEPDispatcherImplNormal(_DeepEPDispatcherImplBase):
         if (
             deep_gemm_wrapper.ENABLE_JIT_DEEPGEMM
             and not get_moe_runner_backend().is_cutlass()
-            and not envs.SGLANG_DEEPEP_BF16_DISPATCH.get()
         ):
             # TODO hard code 128 block quant,use fp8 communication
             hidden_states = sglang_per_token_group_quant_fp8(
